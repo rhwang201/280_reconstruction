3abe785c0aa3869a3ff0df1c94a664573462bf12
Richard Hwang <ee128-de@hive13.CS.Berkeley.EDU>
2013-10-08 17:07:04 -0700

    Add some semicolons, plot camera positions, include imgs, include results

--- code/find_rotation_translation.m
+++ code/find_rotation_translation.m
@@ -8,11 +8,6 @@ function[R, t] = find_rotation_translation(E)
     R_mat = U * W' * V';
     t{1,1} =  U(:,3);
     t{1,2} = -U(:,3);
-    %t_hat = V * W * S * V';
-    %t_vect = [t_hat(3, 2), -t_hat(3, 1), t_hat(2, 1)]';
-
-    %t{1,1} = t_vect;
-    %t{1,2} = -t_vect;
 
     R{1,1} = R_mat;
 
--- code/plot_3d.m
+++ code/plot_3d.m
@@ -1,4 +1,15 @@
-function [] = plot_3d(points)
-    figure(3);
-    scatter3(points(:,1), points(:,2), points(:,3));
+function [] = plot_3d(points, name, t2)
+    figure;
+    hold on;
+    title(sprintf('Reconstructed point cloud for %s', name));
+
+    % Plot point cloud
+    scatter3(points(:,1), points(:,2), points(:,3), 'blue');
+
+    % Plot camera centers
+    scatter3([0, t2(1,1)], [0, t2(2,1)], [0, t2(3,1)], 'red');
+
+    legend('Points', 'Camera Centers');
+
+    hold off;
 end
